75640fde486e7dba010bac33917e0994edd7cb3d
patte <p@tte.io>
2024-01-29 15:48:19 +0300

    feat: mmap file, delayed utf8 parse, fixed type I48F16

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 [The One Billion Row Challenge](https://github.com/gunnarmorling/1brc).
 
 Solutions:
-- [With rayon](./rayon)
-- [With polars](./polars)
+- [With rayon](./rayon) (~50s)
+- [With polars](./polars) (~400s)
--- rayon/src/main.rs
+++ rayon/src/main.rs
@@ -1,33 +1,63 @@
+#![feature(slice_split_once)]
+
+use fixed::types::I48F16;
+use memmap2::Mmap;
 use rayon::prelude::*;
 use std::collections::HashMap;
-use std::io::{BufReader, Read};
+use std::fs::File;
+type Value = I48F16;
+
+// https://gist.github.com/Lucretiel/b9d8a2f75c445ba62035fd80adb5fd57
+fn fast_parse(input: &[u8]) -> Value {
+    let neg = input[0] == b'-';
+    let len = input.len();
+
+    let (d1, d2, d3) = match (neg, len) {
+        (false, 3) => (0, input[0] - b'0', input[2] - b'0'),
+        (false, 4) => (input[0] - b'0', input[1] - b'0', input[3] - b'0'),
+        (true, 4) => (0, input[1] - b'0', input[3] - b'0'),
+        (true, 5) => (input[1] - b'0', input[2] - b'0', input[4] - b'0'),
+        _ => unreachable!(),
+    };
+
+    let int = (d1 as i16 * 100) + (d2 as i16 * 10) + (d3 as i16);
+    let int = if neg { -int } else { int };
+
+    Value::from_num(int) / Value::from_num(10)
+}
 
 fn main() {
     // read stdin into a buffer
     //let mut buffer = Vec::<u8>::with_capacity(4 * 1024 * 1024 * 1024);
     //io::stdin().lock().read_to_end(&mut buffer).unwrap();
 
-    // read the file measure.csv into a buffer completely
-    let mut buffer = Vec::<u8>::with_capacity(4 * 1024 * 1024 * 1024);
-    let mut file = BufReader::new(std::fs::File::open("../measurements-1000000000.txt").unwrap());
-    file.read_to_end(&mut buffer).unwrap();
+    let path = "../measurements-1000000000.txt";
+    let file = File::open(path).expect("failed to open file");
+
+    // read the file measure.csv into a vec
+    //let mut file_vec = Vec::<u8>::with_capacity(4 * 1024 * 1024 * 1024);
+    //let mut file_buffer = BufReader::new(file);
+    //file_buffer.read_to_end(&mut file_vec).unwrap();
+
+    let mapped_data = unsafe { Mmap::map(&file) }.expect("failed to create memory map");
+    let raw_data = &*mapped_data;
+    let raw_data = raw_data.strip_suffix(b"\n").unwrap_or(raw_data);
 
     // group by station into a hashmap
-    let stations_with_values: HashMap<String, Vec<f32>> = buffer
-        .par_split(|&c| c == b'\n')
-        .map(|line_bytes| String::from_utf8(line_bytes.to_vec()).unwrap())
+    let stations_with_values: HashMap<&[u8], Vec<Value>> = raw_data
+        .par_split(|&b| b == b'\n')
+        .map(|line_bytes| {
+            let (station, value_raw) = line_bytes
+                .split_once(|&b| b == b';')
+                .expect("no ; separator");
+            let value = fast_parse(value_raw);
+            (station, value)
+        })
         .fold(
             || HashMap::new(),
-            |mut acc, line| {
-                if line.len() == 0 {
-                    return acc;
-                }
-                let (station, value_raw) = line.split_once(';').unwrap();
-                let value = value_raw.parse::<f32>().unwrap();
-                acc.entry(station.to_string())
-                    .or_insert_with(Vec::new)
-                    .push(value);
-                acc
+            |mut map, (station, value)| {
+                map.entry(station).or_insert_with(Vec::new).push(value);
+                map
             },
         )
         .reduce_with(|mut acc, mut map| {
@@ -39,12 +69,15 @@ fn main() {
         .unwrap();
 
     // release buffer
-    drop(buffer);
+    //drop(file_vec);
 
-    let mut result: Vec<(String, (f32, f32, f32))> = stations_with_values
+    let mut result: Vec<(&&[u8], (Value, Value, Value))> = stations_with_values
         .par_iter()
         .map(|(station, values)| {
-            let avg = ((values.iter().sum::<f32>() / values.len() as f32) * 10.0).round() / 10.0;
+            let avg = ((values.iter().sum::<Value>() / Value::from_num(values.len()))
+                * Value::from_num(10))
+            .round()
+                / Value::from_num(10);
             let min = values
                 .iter()
                 .min_by(|a, b| a.partial_cmp(b).unwrap())
@@ -53,7 +86,7 @@ fn main() {
                 .iter()
                 .max_by(|a, b| a.partial_cmp(b).unwrap())
                 .unwrap();
-            (station.clone(), (avg, *min, *max))
+            (station, (avg, *min, *max))
         })
         .collect();
 
@@ -64,7 +97,13 @@ fn main() {
 
     // print result
     for (station, (avg, min, max)) in &result {
-        println!("{};{}/{}/{}", station, min, avg, max);
+        println!(
+            "{};{}/{}/{}",
+            String::from_utf8_lossy(station),
+            avg,
+            min,
+            max
+        );
     }
 
     println!("Number of stations: {}", result.len());
